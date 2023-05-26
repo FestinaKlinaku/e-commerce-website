fa9aa4e1838998b51568e5374203a378fb92c763
Festina Klinaku <festinaklinaku@gmail.com>
2023-05-26 16:35:55 +0200

    Created a cosmetics e-commerce website and added it's main functionalities

---
+++ about.php
@@ -0,0 +1,38 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+    <?php include 'includes/navbar.php'; ?>
+     
+    <div class="content-wrapper">
+        <div class="container">
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="row">
+                    <div class="col-sm-9" style="text-align: justify; padding-right: 70px">
+                        <h1 class="page-header">About Us</h1>
+                        <p>Welcome to our e-commerce cosmetics company!</p>
+                        <p>At Ecommerce Cosmetics Company, we are passionate about beauty and dedicated to providing high-quality cosmetic products to our customers.</p>
+                        <p>Our mission is to help individuals enhance their natural beauty and express their unique style through our carefully curated collection of cosmetics.</p>
+                        <p>With a focus on quality and innovation, we source our products from trusted brands and manufacturers known for their excellence in the cosmetic industry.</p>
+                        <p>Whether you're looking for skincare essentials, makeup must-haves, or beauty tools, we've got you covered. Our extensive product range includes a variety of options to suit different skin types, tones, and preferences.</p>
+                        <p>Customer satisfaction is at the heart of our business. We strive to provide an exceptional shopping experience, offering easy and secure online transactions, prompt delivery, and excellent customer support.</p>
+                        <p>Thank you for choosing Ecommerce Cosmetics Company for your beauty needs. We are excited to be a part of your beauty journey and look forward to serving you with the best cosmetic products and services.</p>
+                    </div>
+                    <div class="col-sm-3">
+                        <?php include 'includes/sidebar.php'; ?>
+                    </div>
+                </div>
+            </section>
+             
+        </div>
+    </div>
+  
+    <?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
